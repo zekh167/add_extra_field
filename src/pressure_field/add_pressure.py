3fe94ab18876f1043cfba1149205116ddd382ee3
mahyar-osn <m.osanlouy@auckland.ac.nz>
2020-04-03 15:49:07 +1300

    Added methods to merge extra fields with node + small changes.

--- src/pressure_field/add_pressure.py
+++ src/pressure_field/add_pressure.py
@@ -4,46 +4,80 @@ import argparse
 from opencmiss.zinc.context import Context
 from opencmiss.zinc.field import Field
 from opencmiss.zinc.node import Node
-from opencmiss.utils.zinc.field import findOrCreateFieldFiniteElement
+from opencmiss.utils.zinc.field import create_field_finite_element
 
 
 class ProgramArguments(object):
     pass
 
 
-def addPressureField(field, nodeset='nodes'):
-    fm = field.getFieldmodule()
-    fm.beginChange()
-    cache = fm.createFieldcache()
-    if nodeset == 'nodes':
-        nodes = fm.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_NODES)
-    else:
-        nodes = fm.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_DATAPOINTS)
-    pressureField = findOrCreateFieldFiniteElement(fm, name='pressure', components_count=1, type_coordinate=False)
-    pressureNodetemplate = nodes.createNodetemplate()
+def add_field(field_module, field_name, field_value):
+    field_module.beginChange()
 
-    node_iter = nodes.createNodeiterator()
+    cache = field_module.createFieldcache()
+    node_set = field_module.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_NODES)
+    node_iter = node_set.createNodeiterator()
     node = node_iter.next()
 
     while node.isValid():
-        pressureNodetemplate.defineFieldFromNode(pressureField, node)
-        # node.merge(pressureNodetemplate)
+        merge_fields_with_nodes(field_module, node, field_name, field_value, node_set)
         cache.setNode(node)
-        pressureField.setNodeParameters(cache, -1, Node.VALUE_LABEL_VALUE, 1, 1000)
         node = node_iter.next()
-    fm.endChange()
+
+    del cache
+
+    # cache = field_module.createFieldcache()
+    # mesh = field_module.findMeshByDimension(2)
+    # element_iter = mesh.createElementiterator()
+    # element = element_iter.next()
+    #
+    # while element.isValid():
+    #     merge_fields_with_elements(field_module, mesh, element, field_name)
+    #     cache.setElement(element)
+    #     element = element_iter.next()
+
+    field_module.endChange()
+
+
+def merge_fields_with_nodes(field_module, node, field_name, field_value, node_set):
+    field_cache = field_module.createFieldcache()
+    node_template = node_set.createNodetemplate()
+
+    field = field_module.findFieldByName(field_name)
+    node_template.defineField(field)
+    field_cache.setNode(node)
+    node.merge(node_template)
+    if isinstance(field_value, ("".__class__, u"".__class__)):
+        field.assignString(field_cache, field_value)
+    elif isinstance(field_value, list):
+        field.assignReal(field_cache, field_value)
+    else:
+        pass
+
+
+def merge_fields_with_elements(field_module, mesh, element, field_name):
+    #  TODO: adding the field to the elements (1D, 2D, and 3D?)
+
+    field_cache = field_module.createFieldcache()
+    element_template = mesh.createElementtemplate()
+    field = field_module.findFieldByName(field_name)
+    element_template.defineFieldElementConstant(field, -1)
+    field_cache.setElement(element)
+    element.merge(element_template)
+
+    return
 
 
 class AddExtraField(object):
 
-    def __init__(self, filename, output_filename):
+    def __init__(self, filename, field_info, output_filename):
         self._context = Context("AddField")
         self._region = self._context.getDefaultRegion()
         self._region.readFile(filename)
 
         self._output_file = output_filename
 
-        self._add_field()
+        self._add_field(field_info)
 
     def _get_mesh(self):
         fm = self._region.getFieldmodule()
@@ -53,10 +87,13 @@ class AddExtraField(object):
                 return mesh
         raise ValueError('Model contains no mesh')
 
-    def _add_field(self):
+    def _add_field(self, field_info):
         fm = self._region.getFieldmodule()
-        addPressureField(fm.findFieldByName("pressure"),
-                          nodeset='nodes')
+
+        for field_name in field_info:
+            create_field_finite_element(fm, field_name, 1, type_coordinate=False)
+            field_value = field_info[field_name]
+            add_field(fm, field_name, field_value)
 
         self._write()
 
@@ -70,19 +107,19 @@ def main():
         if args.output_ex is None:
             filename = os.path.basename(args.input_ex)
             dirname = os.path.dirname(args.input_ex)
-            output_ex = os.path.join(dirname, filename.split('.')[0] + '_test.' + filename.split('.')[1])
+            output_ex = os.path.join(dirname, filename.split('.')[0] + '_extra_field.' + filename.split('.')[1])
         else:
             output_ex = args.output_ex
 
-    teg = AddExtraField(args.input_ex, output_ex)
+    teg = AddExtraField(args.input_ex, {'pressure': [1000]}, output_ex)
     # teg.add_field()
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Transform (currently only scale) EX data.")
+    parser = argparse.ArgumentParser(description="Add an extra field to an existing EX mesh file.")
     parser.add_argument("input_ex", help="Location of the input EX file.")
     parser.add_argument("-o", "--output-ex", help="Location of the output ex file. "
-                                            "[defaults to the location of the input file if not set.]")
+                                                  "[defaults to the location of the input file if not set.]")
 
     program_arguments = ProgramArguments()
     parser.parse_args(namespace=program_arguments)
